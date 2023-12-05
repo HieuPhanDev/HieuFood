56264151a341d88f55a01d0b58c2d117273eeeff
hieuphandev <phanvanhieu16032003@gmail.com>
2023-12-05 21:20:24 +0700

    first commit

---
+++ javascript/scroll-top.js
@@ -0,0 +1,15 @@
+  $('.scroll').fadeOut()
+
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 160) {
+          $('.scroll').fadeIn();
+      } else {
+          $('.scroll').fadeOut();
+      }
+  });
+
+  $(".scroll").click(function() {
+      $("html, body").animate({
+          scrollTop: 0
+      }, 500);
+  });
