eab4ce2a32fabec470419f2619161e915ba4eedb
hieuphandev <phanvanhieu16032003@gmail.com>
2023-12-20 22:54:45 +0700

    final 1

--- javascript/typed.js
+++ javascript/typed.js
@@ -1,6 +1,6 @@
 // <!-- Swiper JS -->
 var typed = new Typed(".header__logo-text", {
-    strings: ["", "Welcome to HieuFood", "I'm Phan Hieu", "Enjoy your meal !"],
+    strings: ["", "Welcome to PeakyFood", "Enjoy your meal !", "We're Peaky Group !"],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true,
