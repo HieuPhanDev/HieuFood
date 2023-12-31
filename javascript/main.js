cb014511c47b208b71368df0609ecb814eb55625
hieuphandev <phanvanhieu16032003@gmail.com>
2023-12-31 16:22:08 +0700

    final

--- javascript/main.js
+++ javascript/main.js
@@ -1,61 +1,52 @@
 // CHANGE PIICTURE
 
-const bigImg = document.querySelector('.detail-img')
-const smallImg = document.querySelectorAll('.detail-small-img')
+const bigImg = document.querySelector(".detail-img");
+const smallImg = document.querySelectorAll(".detail-small-img");
 
 smallImg.forEach((imgItem) => {
     // console.log(imgItem);
-    imgItem.addEventListener('click', function() {
-        bigImg.src = imgItem.src
-    })
-})
+    imgItem.addEventListener("click", function () {
+        bigImg.src = imgItem.src;
+    });
+});
 
 // -----------------------------------------------------------------------------
 //                  BUY PRODUCT
-const result = document.querySelector('#number-quality')
-var resultNum = result.value
-const btn1 = document.querySelector('.btn1')
-const btn2 = document.querySelector('.btn2')
-const buyBtn = document.querySelector('.order')
+const result = document.querySelector("#number-quality");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
 
-btn1.addEventListener('click', function() {
-    if (!isNaN(resultNum) && result.value > 1) {
-        result.value--
-    }
-    console.log(result.value)
-    return false;
-})
-
-result.onchange = function() {
-    if (this.value == 0) {
-        this.value = 1
-    }
-}
-result.onkeypress = function() {
-    if (isNaN(this.value + String.fromCharCode(event.keyCode))) {
-        return false;
-    }
-}
-btn2.addEventListener('click', function() {
-    if (!isNaN(resultNum)) {
-        result.value++
+btn1.addEventListener("click", function () {
+    if (!isNaN(parseInt(result.value)) && result.value > 1) {
+        result.value--;
     }
     return false;
-})
+});
 
+result.onchange = function () {
+    if (this.value <= 0||isNaN(this.value)) {
+        this.value = 1;
+    }
+};
+btn2.addEventListener("click", function () {
+    if (!isNaN(parseInt(result.value))) {
+        result.value++;
+    }
+    return false;
+});
 
 // ---------------------------------------------------------------------------------
-//                          MENU TAB 
+//                          MENU TAB
 const menuTab = document.querySelector(".tab-list");
-menuTab.addEventListener('click', function(e) {
-    if (e.target.classList.contains('tab-item')) {
-        const target = e.target.getAttribute("data-target")
+menuTab.addEventListener("click", function (e) {
+    if (e.target.classList.contains("tab-item")) {
+        const target = e.target.getAttribute("data-target");
         console.log(target);
-        menuTab.querySelector(".active").classList.remove('active');
-        e.target.classList.add('active');
+        menuTab.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
 
-        const tab = document.querySelector('.product-tap')
-        tab.querySelector(".tab-content.active").classList.remove('active');
-        tab.querySelector(target).classList.add('active');
+        const tab = document.querySelector(".product-tap");
+        tab.querySelector(".tab-content.active").classList.remove("active");
+        tab.querySelector(target).classList.add("active");
     }
-})
+});
--- javascript/scroll-top.js
+++ javascript/scroll-top.js
@@ -1,15 +1,18 @@
-  $('.scroll').fadeOut()
+$(".scroll").fadeOut();
 
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 160) {
-          $('.scroll').fadeIn();
-      } else {
-          $('.scroll').fadeOut();
-      }
-  });
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 160) {
+        $(".scroll").fadeIn();
+    } else {
+        $(".scroll").fadeOut();
+    }
+});
 
-  $(".scroll").click(function() {
-      $("html, body").animate({
-          scrollTop: 0
-      }, 500);
-  });
+$(".scroll").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: 0,
+        },
+        500
+    );
+});
